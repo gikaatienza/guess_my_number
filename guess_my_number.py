86f8e24874cdaa4691ec5d6275855e7112772e61
Angelika Atienza <angelika.atienza@epfl.ch>
2020-10-06 09:45:49 +0200

    Transformation du code avec l’ajout de la classe GuessMachine

--- guess_my_number.py
+++ guess_my_number.py
@@ -8,23 +8,44 @@ Created on Tue Oct  6 08:55:09 2020
 
 import random
 
+
 MIN = 0
 MAX = 1000
 
-if __name__ == '__main__':
-    number_to_guess = random.randint(MIN, MAX)
-    print('Hey! Try to guess a number between %d  and %d!' %(MIN, MAX))
+class GuessMachine():
     
-    while True: 
+    def __init__(self):
+        
+        self._number_to_guess = random.randint(MIN, MAX)
+        self.number_of_attempt = 0
+        
+    def guess(self, num):
+        
+        self.number_of_attempt +=1
+        if num < self._number_to_guess:
+            return 'too low'
+        elif num > self._number_to_guess:
+            return 'too high'
+        else: 
+            return 'found'
+        
+
+if __name__ == '__main__':
+    guess_machine = GuessMachine()
+    print('Hey! Try to guess a number between %d  and %d!' %(MIN, MAX))
+
+    while True:
         user_input = input('Your guess?')
         try:
             user_attempt = int(user_input)
-            if user_attempt == number_to_guess:
-                print('Fantastic, you could find the number I had in mind!')
+            result = guess_machine.guess(user_attempt)
+            if result == 'found':
+                print(
+                    'Fantastic, you find'
+                    'the number I had in mind'
+                    'in %d attempts!' % guess_machine.number_of_attempt)
                 break
-            elif user_attempt < number_to_guess:
-                print('too low')
             else:
-                print('too high')
+                print(result)
         except ValueError:
             print('You joker...that was not an integer!')
