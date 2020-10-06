ccdfba42ad5fafdf398a8d66fcf78185b5906dc0
Angelika Atienza <angelika.atienza@epfl.ch>
2020-10-06 09:02:36 +0200

    Ajout de guess_my_number.py pour une interaction avec l’utilisateur.

---
+++ guess_my_number.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  6 08:55:09 2020
+
+@author: Angelika
+"""
+
+import random
+
+MIN = 0
+MAX = 1000
+
+if __name__ == '__main__':
+    number_to_guess = random.randint(MIN, MAX)
+    print('Hey! Try to guess a number between %d  and %d!' %(MIN, MAX))
+    
+    while True: 
+        user_input = input('Your guess?')
+        try:
+            user_attempt = int(user_input)
+            if user_attempt == number_to_guess:
+                print('Fantastic, you could find the number I had in mind!')
+                break
+            elif user_attempt < number_to_guess:
+                print('too low')
+            else:
+                print('too high')
+        except ValueError:
+            print('You joker...that was not an integer!')
